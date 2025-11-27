95597d76f23cd76abb1e3104c2f7e02c1b5aecf4
badjoke-lab <badjoke.lab@gmail.com>
2025-11-27 21:12:13 +0900

    Configure Leaflet asset handling

--- next.config.js
+++ next.config.js
@@ -1,9 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    appDir: true
-  }
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(png|jpg|gif|svg)$/i,
+      type: 'asset/resource',
+    });
+    return config;
+  },
 };
 
 module.exports = nextConfig;
